f6af7d5430c59eae34469bfc62683949a7a2c89a
Anastasiia Galeeva <Anastasiia.Galeeva@edu.gbjb.ch>
2026-03-08 14:06:42 +0100

    j'ai essayé avec un code très simple mais il y avait pas de pybricks

--- Code_robot/main.py
+++ Code_robot/main.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+#!/usr/bin/env pybricks-micropython 
+# eh bah le robot a pas de pybricks installé
 
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
+from pybricks.parameters import Port, Stop
+from pybricks.tools import wait
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
@@ -14,7 +12,16 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # Create your objects here.
 ev3 = EV3Brick()
-
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.D)
+color_sensor = ColorSensor(Port.S1)      
+distance_sensor = UltrasonicSensor(Port.S4)
 
 # Write your program here.
-ev3.speaker.beep()
+
+# Avancer
+left_motor.run(300)      
+right_motor.run(300)
+wait(3000)               
+left_motor.stop(Stop.BRAKE)
+right_motor.stop(Stop.BRAKE)
