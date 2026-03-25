a5051b1e1ad6a80e6970717b197f1e92c1691c65
Anastasiia Galeeva <Anastasiia.Galeeva@edu.gbjb.ch>
2026-03-25 20:51:56 +0100

    un programme TRES simple

--- Code_robot/main.py
+++ Code_robot/main.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env pybricks-micropython 
 # eh bah le robot a pas de pybricks installé
 
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
-from pybricks.parameters import Port, Stop
+from pybricks.hub import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor, TouchSensor, GyroSensor
+from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
+from pybricks.robotics import DriveBase
 
 # Create your objects here.
 ev3 = EV3Brick()
@@ -19,9 +16,22 @@ distance_sensor = UltrasonicSensor(Port.S4)
 
 # Write your program here.
 
-# Avancer
-left_motor.run(300)      
-right_motor.run(300)
-wait(3000)               
-left_motor.stop(Stop.BRAKE)
-right_motor.stop(Stop.BRAKE)
+# le beep pour tester
+ev3.speaker.beep()
+
+# Boucle principale
+while True:
+    
+    distance = distance_sensor.distance()
+
+    if distance < 200:  # obstacle à moins de 20 cm
+        left_motor.stop(Stop.BRAKE)
+        right_motor.stop(Stop.BRAKE)
+        ev3.speaker.beep()
+        break
+    
+    else:
+        left_motor.run(300)
+        right_motor.run(300)
+
+    wait(100)
